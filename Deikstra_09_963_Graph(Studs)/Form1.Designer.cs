c807c7b3c0f0d4a55cf8ba2b9ef7eae6dde2541b
Smak80 <smaklets@kpfu.ru>
2021-03-01 15:41:18 +0300

    Добавьте файлы проекта.

---
+++ Deikstra_09_963_Graph(Studs)/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Deikstra_09_963_Graph_Studs_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudFrom = new System.Windows.Forms.NumericUpDown();
+            this.nudTo = new System.Windows.Forms.NumericUpDown();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.BackColor = System.Drawing.Color.White;
+            this.mainPanel.Location = new System.Drawing.Point(13, 13);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(759, 477);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnLoad.Location = new System.Drawing.Point(13, 526);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(123, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Загрузить...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.Enabled = false;
+            this.btnFind.Location = new System.Drawing.Point(648, 525);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(123, 23);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "Найти путь";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 530);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "От вершины";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 530);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "До вершины";
+            // 
+            // nudFrom
+            // 
+            this.nudFrom.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.nudFrom.Enabled = false;
+            this.nudFrom.Location = new System.Drawing.Point(311, 528);
+            this.nudFrom.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudFrom.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudFrom.Name = "nudFrom";
+            this.nudFrom.Size = new System.Drawing.Size(60, 23);
+            this.nudFrom.TabIndex = 5;
+            this.nudFrom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudTo
+            // 
+            this.nudTo.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.nudTo.Enabled = false;
+            this.nudTo.Location = new System.Drawing.Point(461, 528);
+            this.nudTo.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTo.Name = "nudTo";
+            this.nudTo.Size = new System.Drawing.Size(61, 23);
+            this.nudTo.TabIndex = 6;
+            this.nudTo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(13, 508);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.nudTo);
+            this.Controls.Add(this.nudFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.mainPanel);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Поиск кратчайшего пути в графе между вершинами";
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudFrom;
+        private System.Windows.Forms.NumericUpDown nudTo;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Deikstra_09_963_Graph(Studs)/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Deikstra_09_963;
+
+namespace Deikstra_09_963_Graph_Studs_
+{
+    public partial class Form1 : Form
+    {
+
+        private GraphPainter gp = null;
+        private Graph g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs ea)
+        {
+            try
+            {
+                var file = GetGraphFileName();
+                if (file != null && File.Exists(file))
+                {
+                    var data = Loader.LoadData(file);
+                    g = new Graph(data);
+                    gp = new GraphPainter(g);
+                    nudTo.Value = nudTo.Maximum = nudFrom.Maximum = g.VertexCount;
+                    nudTo.Enabled = nudFrom.Enabled = btnFind.Enabled = true;
+                    mainPanel.Refresh();          
+                }
+                
+                /*int b = 1;
+                int e = 6;
+                var path = g.GetShortestPath(b, e);
+                ShowPath(path);
+                Console.WriteLine("Длина кратчайшего пути: {0}", Graph.GetShortestPathLength(path));
+                b = 2;
+                path = g.GetShortestPath(b, e);
+                ShowPath(path);
+                Console.WriteLine("Длина кратчайшего пути: {0}", Graph.GetShortestPathLength(path));*/
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Проблема с чтением из файла :(");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Что-то пошло не так :(");
+            }
+        }
+
+        private string GetGraphFileName()
+        {
+            return "data.csv";
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            gp?.Paint(mainPanel.CreateGraphics());
+        }
+    }
+    
+}
